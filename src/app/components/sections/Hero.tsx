2ef0b71241139b6d1aac95880d10095e76373950
PiyushSahu <piyushsahuys@gmail.com>
2025-05-11 22:30:29 +0530

    setting up nextjs project

---
+++ src/app/components/sections/Hero.tsx
@@ -0,0 +1,9 @@
+export  function HeroSection(){
+    return (
+        <div>
+
+            <h1 className="text-2xl text-center text-white">Web Developer Internship Opportunities <br /> at Cognifyz Technologies</h1>
+
+        </div>
+    )
+}
